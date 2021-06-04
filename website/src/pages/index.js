4140afd1053aaaebc48415f10b73993350c81558
anasoid <oufdou.anas@gmail.com>
2021-06-04 19:38:45 +0100

    correct link

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/users/intro">
+            to="/docs/tuto/intro">
             Jmeter as code Tutorial - 5min ⏱️
           </Link>
         </div>
